8656818471c717baf59d5d4471734c8410137314
xiaotongMaxm <42880672+xiaotongMaxm@users.noreply.github.com>
2019-02-22 13:06:55 -0500

    Delete background.js

--- chromeExtension/changeColor/background.js
+++
@@ -1,9 +0,0 @@
-chrome.browserAction.onClicked.addListener(buttonClicked)
-
-function buttonClicked(tab){
-  console.log(tab)
-  let msg = {
-    txt: "hello"
-  }
-  chrome.tabs.sendMessage(tab.id, msg);
-}
